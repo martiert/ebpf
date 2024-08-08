60a97c216cc9a2ab50f2b6f366ff4c14b4711d53
Martin Ertsås <martiert@gmail.com>
2024-08-08 13:00:07 +0200

    Make an example which just logs

---
+++ examples/logging/exec.bpf.c
@@ -0,0 +1,25 @@
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+#define MAX_COMMAND 256
+
+SEC("tp/sched/sched_process_exec")
+int handle_execve(struct trace_event_raw_sched_process_exec * ctx)
+{
+    char command[MAX_COMMAND];
+    unsigned fname_off;
+    fname_off = ctx->__data_loc_filename & 0xFFFF;
+    bpf_probe_read_str(command, MAX_COMMAND, (void*)ctx + fname_off);
+
+    struct task_struct * task = (struct task_struct*) bpf_get_current_task();
+
+    u64 tgid = bpf_get_current_pid_tgid() >> 32;
+    int pid = tgid;
+    int ppid = BPF_CORE_READ(task, real_parent, tgid);
+    bpf_printk("Execing %s pid: %d ppid: %d", command, pid, ppid);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
