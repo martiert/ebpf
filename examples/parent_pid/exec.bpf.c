d8a48b8758035704b0e2541380c7dc4776c98d57
Martin Ertsås <martiert@gmail.com>
2024-08-08 14:07:52 +0200

    Control program using bss entry

---
+++ examples/parent_pid/exec.bpf.c
@@ -0,0 +1,30 @@
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+#define MAX_COMMAND 256
+
+SEC(".bss")
+int parent_pid = 0;
+
+SEC("tp/sched/sched_process_exec")
+int handle_execve(struct trace_event_raw_sched_process_exec * ctx)
+{
+    struct task_struct * task = (struct task_struct*) bpf_get_current_task();
+    u64 tgid = bpf_get_current_pid_tgid() >> 32;
+    int pid = tgid;
+    int ppid = BPF_CORE_READ(task, real_parent, tgid);
+    if (ppid != parent_pid)
+        return 0;
+
+    char command[MAX_COMMAND];
+    unsigned fname_off;
+    fname_off = ctx->__data_loc_filename & 0xFFFF;
+    bpf_probe_read_str(command, MAX_COMMAND, (void*)ctx + fname_off);
+
+    bpf_printk("Execing %s pid: %d ppid: %d", command, pid, ppid);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
