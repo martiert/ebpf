5ed706ff30cd9f54e8dd69535fcf63adc988f036
Martin Ertsås <martiert@gmail.com>
2024-08-20 12:54:53 +0200

    Continue making images

--- presentation/examples/sched_process_exit.c
+++ presentation/examples/sched_process_exit.c
@@ -1,10 +1,10 @@
 SEC("tp/sched/sched_process_exit")
 int handle_exit(void *)
 {
-  pid_t pid =
-      bpf_get_current_pid_tgid() >> 32;
-  if (bpf_map_delete_elem(&monitored,
-              &pid) != 0)
+  pid_t tgid = bpf_get_current_pid_tgid();
+  pid_t pid = tgid >> 32;
+  if (tgid != pid && 
+    bpf_map_delete_elem(&monitored, &pid) != 0)
       return 0;
 
   struct event event={0};
